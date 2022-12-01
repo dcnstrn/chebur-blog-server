4c54d451dbf66544742d247f321ebbf8d048a7c6
deconsctruction.events <sanimw3@gmail.com>
2022-12-01 14:01:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors,  Pos
 app.delete('/posts/:id', checkAuth, PostController.remove)
 app.patch('/posts/:id', postCreateValidation, handleValidationErrors,  PostController.update)
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.log(err)
     }
